4c4054c447f6c9ac0f8c0248153f2dd64f9ff257
Nikolay <nik.chi09@gmail.com>
2026-03-16 17:19:36 +0300

    Update hello_world.cpp: add user input

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-int main() {
-  string name;
-  cout << "Enter username: ";
-  cin >> name;
-  cout << "Hello world from " << name << endl;
-  return 0;
-}
+using namespace std;                         
+int main()
+{    string name;    
+    cout << "Enter your name: ";
+    cin >> name;    
+    cout << "Hello world from " << name << endl;
+        return 0;}
